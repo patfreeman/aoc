d4769afada1fbd77e1be086824ebbae3ab33e25e
Patrick Freeman <github@e2r4.com>
2017-05-03 15:31:10 -0500

    argparse and verbosity improvements for day2

--- day2a.py
+++ day2a.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
+import argparse
 import sys
 
-keypad = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+parser = argparse.ArgumentParser(description='Finds the bathroom code')
+parser.add_argument('input_file', metavar='<inputfile>', type=str, default='input2.txt', nargs = '?',
+		help='the name of the input file. defaults to input2.txt')
+parser.add_argument('-v', '--verbose', action='store_true',
+		help='enable verbose output')
+args = parser.parse_args()
 
-fp = open('input2.txt', 'r')
+code = ""
+keypad = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
+fp = open(args.input_file, 'r')
+
+if args.verbose:
+	print "using keypad:"
+	for line in keypad:
+		print line
+
 for line in fp.readlines():
 	x = 0
 	y = 0
-	location = "5"
-	for dir in line:
+	if args.verbose:
+		print "Starting at location " + str(keypad[y+1][x+1]) + ", coordinates [" + str(x) + "," + str(y) + "]"
+	for dir in line.strip():
 		if dir == "L":
 			x -= 1
-		if dir == "R":
+		elif dir == "R":
 			x += 1
-		if dir == "U":
+		elif dir == "U":
 			y -= 1
-		if dir == "D":
+		elif dir == "D":
 			y += 1
+		else:
+			print "Skipping unknown direction \"" + dir + "\""
+			continue
 		if x < 0:
 			x = -1
 		if x > 0:
@@ -25,7 +43,8 @@ for line in fp.readlines():
 			y = -1
 		if y > 0:
 			y = 1
-	sys.stdout.write(str(keypad[y+1][x+1]))
-	sys.stdout.flush()
-print ""
+		if args.verbose:
+			print "Moving " + dir + " to location " + str(keypad[y+1][x+1]) + ", coordinates [" + str(x) + "," + str(y) + "]"
+	code = code + str(keypad[y+1][x+1])
+print code
 sys.exit(0)
--- day2b.py
+++ day2b.py
@@ -1,32 +1,50 @@
 #!/usr/bin/env python
+import argparse
 import sys
 
+parser = argparse.ArgumentParser(description='Finds the bathroom code')
+parser.add_argument('input_file', metavar='<inputfile>', type=str, default='input2.txt', nargs = '?',
+		help='the name of the input file. defaults to input2.txt')
+parser.add_argument('-v', '--verbose', action='store_true',
+		help='enable verbose output')
+args = parser.parse_args()
 
+code = ""
 keypad = [ [0, 0, '1', 0, 0],
 	[0, '2', '3', '4', 0],
 	['5', '6', '7', '8', '9'],
 	[0, 'A', 'B', 'C', 0],
 	[0, 0, 'D', 0, 0] ]
 
-fp = open('input2.txt', 'r')
+fp = open(args.input_file, 'r')
+
+if args.verbose:
+	print "using keypad:"
+	for line in keypad:
+		print line
+
 for line in fp.readlines():
 	x = 0
 	y = 2
-	location = "5"
-	for dir in line:
+	if args.verbose:
+		print "Starting at location " + str(keypad[y][x]) + ", coordinates [" + str(x) + "," + str(y) + "]"
+	for dir in line.strip():
 		if dir == "L":
 			if x-1 >= 0 and keypad[y][x-1] != 0:
 				x -= 1
-		if dir == "R":
+		elif dir == "R":
 			if x+1 <= 4 and keypad[y][x+1] != 0:
 				x += 1
-		if dir == "U":
+		elif dir == "U":
 			if y-1 >= 0 and keypad[y-1][x] != 0:
 				y -= 1
-		if dir == "D":
+		elif dir == "D":
 			if y+1 <= 4 and keypad[y+1][x] != 0:
 				y += 1
-	sys.stdout.write(str(keypad[y][x]))
-	sys.stdout.flush()
-print ""
-sys.exit(0)
+		else:
+			print "Skipping unknown direction \"" + dir + "\""
+			continue
+		if args.verbose:
+			print "Moving " + dir + " to location " + str(keypad[y][x]) + ", coordinates [" + str(x) + "," + str(y) + "]"
+	code = code + str(keypad[y][x])
+print code
