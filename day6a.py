29c5cf3e23a3a64c2d0f647230a07d18d797dd0f
Patrick Freeman <github@e2r4.com>
2017-05-02 22:05:12 -0500

    functional day6a

--- day6a.py
+++ day6a.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
-import re
 import string
 import sys
 
-def 
 
 chars = []
-occurance_dict = {}
 fp = open('input6.txt', 'r')
 for line in fp.readlines():
 	idx = 0
@@ -17,10 +14,15 @@ for line in fp.readlines():
 			chars.append(dict((el,0) for el in string.lowercase[:26]))
 		chars[idx][char] += 1
 		idx += 1
-print chars
+
+def top_char(odict):
+	for key in sorted(odict.keys(), reverse=True):
+		for letter in sorted(odict[key]['keys']):
+			return letter
+			
 
 for idx in chars:
-	print "Position " + str(idx)
+	occurance_dict = {}
 	for key, value in sorted(idx.iteritems(), key=lambda (k,v): (v,k)):
 		try:
 			occurance_dict[value]
@@ -33,8 +35,5 @@ for idx in chars:
 			occurance_dict[value]['keys'] = []
 		if value == occurance_dict[value]['value']:
 			occurance_dict[value]['keys'].append(key)
-	
-	for key in sorted(occurance_dict.keys(), reverse=True):
-		for letter in sorted(occurance_dict[key]['keys']):
-			sys.stdout.write(letter)
-			break
+	sys.stdout.write(top_char(occurance_dict))
+print
