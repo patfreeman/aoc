740d962f15d63a0613bf1537bdb6a43a409defd8
Patrick Freeman <github@e2r4.com>
2017-05-03 17:51:32 -0500

    improved day3a.py

--- day3a.py
+++ day3a.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
-import re
-import sys
-
 fp = open('input3.txt', 'r')
-
 cnt = 0
 
 for line in fp.readlines():
-	sides = re.split('\s+', line.strip())
-	if int(sides[0]) < int(sides[1]) + int(sides[2]) and int(sides[1]) < int(sides[0]) + int(sides[2]) and int(sides[2]) < int(sides[0]) + int(sides[1]):
+	sides = sorted(map(int,line.strip().split()), reverse=True)
+	if sides[0] <= sides[1] + sides[2]:
 		cnt+=1
-	print sides[0] + " " + sides[1] + " " + sides[2] + " " + str(cnt)
+	print " ".join(map(str,sides)) + " " + str(cnt)
 print cnt
