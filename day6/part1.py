dcc4fe981b832a266f1d845e13dc4928e2842f68
Patrick Freeman <github@e2r4.com>
2017-05-09 15:29:57 -0500

    argparse and verbosity

--- day6/part1.py
+++ day6/part1.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python
+import argparse
 import string
 import sys
 
-
-chars = []
-fp = open('input6.txt', 'r')
-for line in fp.readlines():
-	idx = 0
-	for char in line.strip():
-		try:
-			chars[idx]
-		except:
-			chars.append(dict((el,0) for el in string.lowercase[:26]))
-		chars[idx][char] += 1
-		idx += 1
+parser = argparse.ArgumentParser(description='Finds the error-crrected message')
+parser.add_argument('input_file', metavar='<inputfile>', type=str, default='input6.txt', nargs = '?',
+		help='the name of the input file. defaults to input6.txt')
+parser.add_argument('-v', '--verbose', action='store_true',
+		help='enable verbose output')
+args = parser.parse_args()
 
 def top_char(odict):
 	for key in sorted(odict.keys(), reverse=True):
 		for letter in sorted(odict[key]['keys']):
 			return letter
 			
+chars = []
+fp = open(args.input_file, 'r')
+for line in fp.readlines():
+	idx = 0
+	if args.verbose:
+		print "Reading line " + line.strip()
+	for char in line.strip():
+		try:
+			chars[idx]
+		except:
+			chars.append(dict((el,0) for el in string.lowercase[:26]))
+		if args.verbose:
+			print "Adding to chars[" + str(idx) + "][" + char + "]"
+		chars[idx][char] += 1
+		idx += 1
+	if args.verbose:
+		print chars
 
 for idx in chars:
 	occurance_dict = {}
