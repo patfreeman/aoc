aa1660308c77983932dbe59eaac1403b92fb53c2
Patrick Freeman <github@e2r4.com>
2017-05-19 11:33:49 -0500

    Clean up useless variable

--- day5/part1.py
+++ day5/part1.py
@@ -3,14 +3,12 @@
 import hashlib
 
 id="ugkcyxxp"
-cnt=0
 i=0
 password=""
-while cnt < 8:
+while len(password) < 8:
 	md5 = hashlib.md5(id + str(i)).hexdigest()
 	if md5[:5] == "00000":
 		print str(i) + " " + md5
 		password = password + md5[5]
-		cnt += 1
 	i += 1
 print password
