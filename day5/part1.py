57c3bf2f1d3c94a6e27bde8b0e99d41f669b34da
Patrick Freeman <github@e2r4.com>
2017-05-19 17:25:39 -0500

    spinner!

--- day5/part1.py
+++ day5/part1.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 
 import hashlib
+import sys
 
 id="ugkcyxxp"
 i=0
+SPINNER_MOD=4500
+spinner=["|","/","-","\\"]
 password=""
 while len(password) < 8:
 	md5 = hashlib.md5(id + str(i)).hexdigest()
 	if md5[:5] == "00000":
-		print str(i) + " " + md5
+		#print str(i) + " " + md5
 		password = password + md5[5]
+		sys.stdout.write("\r")
+		sys.stdout.write(password)
+                sys.stdout.flush()
 	i += 1
-print password
+	if not i % SPINNER_MOD:
+                sys.stdout.write("\b")
+                sys.stdout.write(spinner[(i/SPINNER_MOD)%len(spinner)])
+                sys.stdout.flush()
+sys.stdout.write("\r")
+print "".join(password)
--- day5/part2.py
+++ day5/part2.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
 
 import hashlib
+import sys
 
 id="ugkcyxxp"
 i=0
+SPINNER_MOD=4500
+spinner=["|","/","-","\\"]
 password=[" "," "," "," "," "," "," "," "]
+sys.stdout.write("".join(password) + "|")
 while " " in password:
 	md5 = hashlib.md5(id + str(i)).hexdigest()
 	if md5[:5] == "00000":
-		print str(i) + " " + md5 + " " + "".join(password) + " " + md5[6]
+		#print str(i) + " " + md5 + " " + "".join(password) + " " + md5[6]
 		if ord(md5[5]) < 56:
 			if password[int(md5[5])] == " ":
 				password[int(md5[5])] = md5[6]
+		sys.stdout.write("\r" + "".join(password) + " ")
+		sys.stdout.flush()
 	i += 1
-print "".join(password)
+	if not i % SPINNER_MOD:
+		sys.stdout.write("\b")
+		sys.stdout.write(spinner[(i/SPINNER_MOD)%len(spinner)])
+		sys.stdout.flush()
+sys.stdout.write("\r")
+sys.stdout.write("".join(password))
+sys.stdout.write("\n")
